274752bcd86e8ecb9205dc7127e2e754d9de79f6
Sean Brisbane <s.brisbane1@physics.ox.ac.uk>
2015-09-24 14:00:59 +0100

    xrdp-xsession added which sets the UK keyboard layout.  User must choose to use this in their .xsession

--- manifests/remote_connection.pp
+++ manifests/remote_connection.pp
@@ -24,6 +24,13 @@ class user_software::remote_connection {
    group   => 'root',
    notify => Service['xrdp']
    }
+   file { '/usr/bin/xrdp-xsession':
+   source  => "puppet:///modules/$module_name/xrdp-xsession",
+   ensure  => 'present',
+   mode    => '0755',
+   owner   => 'root',
+   group   => 'root',
+   }
   #This is a hack.  it replaces the Us layout with the UK one as I cant find how to switch to UK one any other way
 #  file { '/etc/xrdp/km-0409.ini':
 #     ensure => link,
